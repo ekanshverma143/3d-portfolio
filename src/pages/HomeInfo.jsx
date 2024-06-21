2e5ad9e98996947c5e3ff14506093c9a320cd2c9
ekansh verma <21bee083@nith.ac.in>
2024-06-21 22:28:12 +0530

    -Email.js setup,-PopUp added in home screen

---
+++ src/pages/HomeInfo.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import {arrow} from "../assets/icons"
+
+const InfoBox = ({text,link,btnText})=>(
+
+    <div className="info-box">
+       <p className="font-medium sm-text-xl text-center">{text}</p>
+        <Link to={link} className="neo-brutalism-white neo-btn">
+            {btnText}
+            <img src={arrow} className="w-4 h-4 object-contain"/>
+         </Link>
+
+    </div>
+)
+
+const renderContent={
+    1: (
+         <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8
+                          text-white mx-5">
+                            Hi, I am <span className="font-semibold">Ekansh Verma</span>👋
+                            <br />
+                        A Web Developer and Designer 
+                            </h1>
+                            
+        ),
+    2: (
+         <InfoBox 
+           text="I am Final Year UnderGrad at National Institute of Technology, Hamirpur (H.P)"
+           link="about"
+            btnText ="Learn More"
+         />
+        
+        ),
+    3: (
+        <InfoBox 
+        text="During my college years, I created and developed multiple projects and designs.Want to know more?"
+        link="/projects"
+        btnText ="Visit my Portfolio"
+        />
+        ),
+    4: (
+         <InfoBox 
+         text="Got an offer I can't refuse? Hit me up and let's chat!"
+         link="/contact"
+         btnText ="Let's Talk !"
+         />
+         ),
+        
+}
+
+
+const HomeInfo = ({currentStage}) => {
+  return renderContent[currentStage] || null;
+}
+
+export default HomeInfo
