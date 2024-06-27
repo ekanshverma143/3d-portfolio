1d0260e05fb32b4a4cd16bd50ae03d008d3ee0f1
ekansh verma <21bee083@nith.ac.in>
2024-06-27 15:02:02 +0530

    alert,contact page completed

--- src/hooks/useAlert.js
+++ src/hooks/useAlert.js
@@ -1,21 +1,12 @@
-import React from 'react'
-import { useState } from 'react'
+import { useState } from 'react';
 
 const useAlert = () => {
-    const  [alert, setalert] = useState({show:'false',text:'',type:'danger'})
+    const [alert, setAlert] = useState({ show: false, text: '', type: 'danger' });
 
-    const showAlert=(text, type='danger')=>({
-        show: 'true',
-        text,
-        type 
+    const showAlert = ({ text, type = 'danger' }) => setAlert({ show: true, text, type });
+    const hideAlert = () => setAlert({ show: false, text: '', type: 'danger' });
 
-    })
-    const hideAlert=()=>setAlert ({
-        show: 'false',
-        text:'',
-        type:'danger'
-    })
-  return { alert,showAlert,hideAlert}
-}
+    return { alert, showAlert, hideAlert };
+};
 
-export default useAlert
+export default useAlert;
